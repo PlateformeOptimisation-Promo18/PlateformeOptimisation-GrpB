c3f05d78d5019e20d20494d4f58ecd4f2ac08aef
Believe it 4 <jade.panchaud@mycarspot.fr>
2020-05-14 19:47:25 +0200

    Ajout de tests

--- src/main/model/algorithmes/fourmis/Pheromones.java
+++ src/main/model/algorithmes/fourmis/Pheromones.java
@@ -1,19 +1,60 @@
 package main.model.algorithmes.fourmis;
 
+import main.model.generic.Solution;
+import main.model.problemes.Problem;
+
+import java.util.List;
+
 public class Pheromones {
 
+    private List<double[]> tracePheromones;
+
+    public void setTracePheromones(List<double[]> tracePheromones) {
+        this.tracePheromones = tracePheromones;
+    }
+
+    public List<double[]> getTracePheromones() {
+        return tracePheromones;
+    }
+
     //TODO
-    public Pheromones() {}
+    public Pheromones(){}
 
     //TODO
     public void nouvelleFourmi(){}
 
     //TODO
-    public void evaporer(double quantiteEvaporation){}
+    public void evaporer(double quantiteEvaporation){
+        for (double[] tache: tracePheromones) {
+            for(int i = 0; i < tache.length; i++){
+                tache[i] -= quantiteEvaporation;
+            }
+        }
+    }
 
     //TODO
-    public void récompenser(double quantitePheromone){}
+    public void recompenser(Problem problem, Solution fourmis, double quantitePheromone) {
+        boolean[] variablesActives = problem.GetActiveVariable(fourmis);
+        int i = 0;
+
+        for (double[] d : tracePheromones) {
+            if(variablesActives[i]){
+
+            }
+        }
+    }
 
     //TODO
-    public void ajuster(double quantiteMinimalePheromone){}
+    public void ajuster(double probaTotale, double quantiteMinimalePheromone){
+        for (double[] tache: tracePheromones) {
+            for (double proba: tache) {
+                proba = proba/probaTotale;
+                if(proba < quantiteMinimalePheromone){
+                    proba += (quantiteMinimalePheromone - proba);
+                }
+            }
+        }
+    }
+
+
 }
--- src/test/model/algorithmes/fourmis/PheromonesTest.java
+++ src/test/model/algorithmes/fourmis/PheromonesTest.java
@@ -1,9 +1,80 @@
 package test.model.algorithmes.fourmis;
 
-import org.junit.Test;
+import main.model.algorithmes.fourmis.Pheromones;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PheromonesTest {
+    Pheromones pheromones;
+    @BeforeEach
+    void setUp(){
+        pheromones = new Pheromones();
+        List<double[]> setUp = new LinkedList<>();
+        double[] tab2 = {0.5,0.5};
+        double[] tab3 = {1/3, 1/3, 0.333};
+        double[] tab2b = {0.5,0.5};
+        setUp.add(tab2);
+        setUp.add(tab3);
+        setUp.add(tab2b);
+        pheromones.setTracePheromones(setUp);
+    }
+
+
+    @Test
+    void proba_should_equals_1(){
+        pheromones.ajuster(1, 0);
+        for (double[] tache: pheromones.getTracePheromones()) {
+            double proba = 0;
+            for(int i = 0; i < tache.length; i++){
+                proba += tache[i];
+            }
+            assertEquals(1.0, proba);
+        }
+    }
+
+    @Test
+    void proba_first_tab_should_evaporate_of_01(){
+        List<double[]> testEvaporate = new LinkedList<>();
+        double[] tab2 = {0.4,0.4};
+        double[] tab3 = {0.233, 0.233, 0.233};
+        testEvaporate.add(tab2);
+        testEvaporate.add(tab3);
+        testEvaporate.add(tab2);
+
+        pheromones.evaporer(0.1);
+        assertArrayEquals(testEvaporate.get(0), pheromones.getTracePheromones().get(0));
+    }
+
+    @Test
+    void proba_second_tab_should_evaporate_of_01(){
+        List<double[]> testEvaporate = new LinkedList<>();
+        double[] tab2 = {0.4,0.4};
+        double[] tab3 = {0.233, 0.233, 0.233};
+        testEvaporate.add(tab2);
+        testEvaporate.add(tab3);
+        testEvaporate.add(tab2);
+
+        pheromones.evaporer(0.1);
+        assertArrayEquals(testEvaporate.get(1), pheromones.getTracePheromones().get(1));
+    }
+
+    @Test
+    void proba_third_tab_should_evaporate_of_01(){
+        List<double[]> testEvaporate = new LinkedList<>();
+        double[] tab2 = {0.4,0.4};
+        double[] tab3 = {0.233, 0.233, 0.233};
+        testEvaporate.add(tab2);
+        testEvaporate.add(tab3);
+        testEvaporate.add(tab2);
+
+        pheromones.evaporer(0.1);
+        assertArrayEquals(testEvaporate.get(2), pheromones.getTracePheromones().get(2));
+    }
 
 }
