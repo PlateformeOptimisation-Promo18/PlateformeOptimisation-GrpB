a9b4c689d2937acd232c2c9af4a7973a530dc00d
Simon <simon.marques@limayrac.fr>
2020-05-26 10:33:09 +0200

    Structure des classes et de leurs méthodes respectives

--- src/main/model/algorithmes/spea2/Archive.java
+++ src/main/model/algorithmes/spea2/Archive.java
@@ -1,7 +1,31 @@
 package main.model.algorithmes.spea2;
 
+
 public class Archive extends Population {
 
+    public Archive(int isArchive){
+
+    }
+
+    public void updateArchive(Population population, int iArchiveSize, Problem pb) {
+
+    }
+
+    private List<Individual> getSet(){
+
+    }
+
+    private void miseAjourDistancePop(Problem pb, Population populationTemp) {
+
+    }
+
+    public void enleveDoublons(){
+
+    }
+
+    public void selection(Population population, InterfaceRandom generator, int iPopSize, int iNumkVoisin) {
+
+    }
 
     public Population selectionParents(int nombreParent){
         // TODO :
--- src/main/model/algorithmes/spea2/Population.java
+++ src/main/model/algorithmes/spea2/Population.java
@@ -1,59 +1,63 @@
 package main.model.algorithmes.spea2;
 
 import javafx.beans.InvalidationListener;
+import java.util.ArrayList;
+
+import com.sun.tools.javac.util.List;
+
 import javafx.beans.Observable;
 
 import java.util.ArrayList;
 
-public class Population implements Observable {
+public class Population{
 
-    private ArrayList<Individu> individus;
+    private List<Individu> set;
 
-    public void Union(Population population) {
-        individus.addAll(population.individus);
-    }
-
-    public Population selection(int nombreSelection) {
-        //TODO :
-        return null;
-    }
-
-    public void evaluerPopulation() {
-        //TODO :
-    }
-
-    public void CalculerFitness() {
-        //TODO :
-    }
-
-    public void CalculerRawFitness() {
-        //TODO :
-    }
-
-    public void CalculerForce() {
-        //TODO :
-    }
-
-    public void CalculerDensité() {
-        //TODO :
-    }
-
-    public void croiserPopulation(float probaCroisement){
-        // TODO :
-    }
-
-    public void muterPopulaiton(float probaMutation, float probaMutaionDesGenes){
-        // TODO :
-    }
-
-
-    @Override
-    public void addListener(InvalidationListener listener) {
+    public Population(int iSize){
 
     }
 
-    @Override
-    public void removeListener(InvalidationListener listener) {
+    protected void add(Individual ind){
+
+    }
+
+    public void initialise(int iPopulationSize, Problem pb, InterfaceRandom generator) throws Exception{
+
+    }
+
+    public void evalue(Problem pb) {
+
+    }
+
+    public Population crossover(double dCrossoverRate, Problem pb, InterfaceRandom generator) {
+
+    }
+
+    private Individual tireParent(Individual ind1, InterfaceRandom generator){
+
+    }
+
+    public void mutation(Population childreen, double dGeneMutation, double dIndividualMutationRate, Problem pb) {
+
+    }
+
+    public int Individual getNbIndividuals(int i) {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public List<Individual> getIndividualSet(){
+
+    }
+
+    public void setIndividualSet(List<Individual> set) {
+
+    }
+
+    public List<Solution> getAssociatedSolutionSet(){
 
     }
 }
