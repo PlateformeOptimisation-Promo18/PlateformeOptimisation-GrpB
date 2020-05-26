adaddcdd0069e607103b85950a3a2d579f162488
Believe it 4 <jade.panchaud@mycarspot.fr>
2020-05-26 17:39:21 +0200

    Revert "Ajout de la fonction pricipale evaluate de Scenario"
    This reverts commit 19e81ac004029c9b58b79250adb1639a7acaf681.
    
    Retour à l'état précédent car la méthode evaluate demande des classes encore non existantes dans la structure.

--- src/main/model/problemes/Scenario.java
+++ src/main/model/problemes/Scenario.java
@@ -6,16 +6,7 @@ import main.model.generic.Solution;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
 
-/**
- * Classe Scenario
- * @author Axel Serieys
- *
- */
 public class Scenario extends Solution {
 
     public Scenario(GraphProject gp) {
@@ -37,49 +28,169 @@ public class Scenario extends Solution {
         valuesObjectives.set(1, iVal);
     }
 
-    /**
-     * Évalue un scénario : détermine le coût et la durée du chemin correspondant
-     * @param gpTest : Le graphe de projet
-     */
     public void evaluate(Problem gpTest) {
-    	GraphProject gp = (GraphProject) gpTest;
-    	//initialisation des listes
-    	List<PlannedNode> listNodeToPlan = new LinkedList<>();
-    	initializeListNodeToPlan(listNodeToPlan, gp);
-    	List<Resource> listAvailableResources = gp.getCopyListInitialResources();
-    	
-    	PriorityQueue<ResourcesPlanned> listResourcesPlanned = new PriorityQueue<>();
-    	SharedEnvironmentForPlanification envir = new SharedEnvironmentForPlanification(listAvailableResources, listResourcesPlanned);
-    	
-    	
-    	while(!listNodeToPlan.isEmpty()) {
-    		PlannedNode noeudCourant = searchForNodeWithoutPreviousNodeAndWithAvailableResources(listNodeToPlan, listAvailableResources);
-    		if (noeudCourant != null) {
-    			Node initialNode = noeudCourant.getInitialNode();
-    			if(noeudCourant.getClassInitialNode == OrNode.class) {
-    				OrNode orNode = (OrNode) initialNode;
-    				int iSelectedOrNodeAlternative = getSelectedOrAlternative(((OrNode) initialNode).getIdOrNode()).getId());
-    				int iIdNodeSelectedAlternative = orNode.getNext(iSelectedOrNodeAlternative);
-    				removeUnselectedBranch(listNodeToPlan, gp, orNode, iIdNodeSelectedAlternative);
-    			} else {
-    				if(noeudCourant.getClassInitialNode() == TaskNode.class) {
-    					taskProcessing(noeudCourant, envir);
-    				}
-    			}
-    			updateNextNodes(listNodeToPlan, noeudCourant);
-    			listNodeToPlan.remove(noeudCourant);
-			} else {
-				ResourcesPlanned releaseResources = listResourcesPlanned.poll();
-				envir.setCurrentDate(releaseResources.getReleaseDate());
-				List<Resource> listToRelease = releaseResources.getListResources();
-				for(Resource res : listToRelease) {
-					listAvailableResources.get(currentResource.getIdResource()).addQuantity(currentResource.getdQuantity());
-				}
-			}
-    	}
-    	setDuration(envir.getEndingDate());
-    	setCost(envir.getdBranchTotalCost());
-    	computeNormalizedObjective(gpTest);
+
+        int tableauEntier[] = {0, 0, 0, 0, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier)) {
+            setDuration(3.8);
+            setCost(17.4);
+        }
+        int tableauEntier1[] = {1, 0, 0, 0, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier1)) {
+            setDuration(9.1);
+            setCost(23.9);
+        }
+
+        int tableauEntier2[] = {0, 1, 0, 0, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier2)) {
+            setDuration(9.8);
+            setCost(18.5);
+        }
+        int tableauEntier3[] = {1, 1, 0, 0, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier3)) {
+            setDuration(9.1);
+            setCost(23.9);
+        }
+        int tableauEntier4[] = {0, 0, 1, 0, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier4)) {
+            setDuration(3.8);
+            setCost(17.4);
+        }
+        int tableauEntier5[] = {1, 0, 1, 0, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier5)) {
+            setDuration(4.3);
+            setCost(21.5);
+        }
+        int tableauEntier6[] = {0, 1, 1, 0, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier6)) {
+            setDuration(9.8);
+            setCost(18.5);
+        }
+        int tableauEntier7[] = {1, 1, 1, 0, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier7)) {
+            setDuration(4.3);
+            setCost(21.5);
+        }
+        int tableauEntier8[] = {0, 0, 0, 1, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier8)) {
+            setDuration(9.1);
+            setCost(16.5);
+        }
+        int tableauEntier9[] = {1, 0, 0, 1, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier9)) {
+            setDuration(14.4);
+            setCost(23.0);
+        }
+        int tableauEntier10[] = {0, 1, 0, 1, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier10)) {
+            setDuration(15.1);
+            setCost(17.6);
+        }
+        int tableauEntier11[] = {1, 1, 0, 1, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier11)) {
+            setDuration(14.4);
+            setCost(23.0);
+        }
+        int tableauEntier12[] = {0, 0, 1, 1, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier12)) {
+            setDuration(9.1);
+            setCost(16.5);
+        }
+        int tableauEntier13[] = {1, 0, 1, 1, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier13)) {
+            setDuration(9.6);
+            setCost(20.6);
+        }
+        int tableauEntier14[] = {0, 1, 1, 1, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier14)) {
+            setDuration(15.1);
+            setCost(17.6);
+        }
+        int tableauEntier15[] = {1, 1, 1, 1, 0};
+        if (Arrays.equals(this.valueVariables, tableauEntier15)) {
+            setDuration(9.6);
+            setCost(20.6);
+        }
+        int tableauEntier16[] = {0, 0, 0, 0, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier16)) {
+            setDuration(3.8);
+            setCost(14.3);
+        }
+        int tableauEntier17[] = {1, 0, 0, 0, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier17)) {
+            setDuration(9.1);
+            setCost(20.8);
+        }
+        int tableauEntier18[] = {0, 1, 0, 0, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier18)) {
+            setDuration(9.8);
+            setCost(15.4);
+        }
+        int tableauEntier19[] = {1, 1, 0, 0, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier19)) {
+            setDuration(9.1);
+            setCost(20.8);
+        }
+        int tableauEntier20[] = {0, 0, 1, 0, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier20)) {
+            setDuration(3.8);
+            setCost(14.3);
+        }
+        int tableauEntier21[] = {1, 0, 1, 0, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier21)) {
+            setDuration(4.3);
+            setCost(18.4);
+        }
+        int tableauEntier22[] = {0, 1, 1, 0, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier22)) {
+            setDuration(9.8);
+            setCost(15.4);
+        }
+        int tableauEntier23[] = {1, 1, 1, 0, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier23)) {
+            setDuration(4.3);
+            setCost(18.4);
+        }
+        int tableauEntier24[] = {0, 0, 0, 1, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier24)) {
+            setDuration(9.1);
+            setCost(13.4);
+        }
+        int tableauEntier25[] = {1, 0, 0, 1, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier25)) {
+            setDuration(14.4);
+            setCost(19.9);
+        }
+        int tableauEntier26[] = {0, 1, 0, 1, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier26)) {
+            setDuration(15.1);
+            setCost(14.5);
+        }
+        int tableauEntier27[] = {1, 1, 0, 1, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier27)) {
+            setDuration(14.4);
+            setCost(19.9);
+        }
+        int tableauEntier28[] = {0, 0, 1, 1, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier28)) {
+            setDuration(9.1);
+            setCost(13.4);
+        }
+        int tableauEntier29[] = {1, 0, 1, 1, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier29)) {
+            setDuration(9.6);
+            setCost(17.5);
+        }
+        int tableauEntier30[] = {0, 1, 1, 1, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier30)) {
+            setDuration(15.1);
+            setCost(14.5);
+        }
+        int tableauEntier31[] = {1, 1, 1, 1, 1};
+        if (Arrays.equals(this.valueVariables, tableauEntier31)) {
+            setDuration(9.6);
+            setCost(17.5);
+        }
     }
 
 
