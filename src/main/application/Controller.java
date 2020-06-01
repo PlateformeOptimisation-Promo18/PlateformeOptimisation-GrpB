f4a64c1ed8533b6d8dba4081ff514b2407d5e8ee
Corentin Pontal <corentin.pontal@laposte.net>
2020-06-01 13:43:04 +0200

    Premier commit branche main de l'interface pour corriger le pb

--- src/main/application/Controller.java
+++ src/main/application/Controller.java
@@ -1,4 +1,37 @@
 package main.application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import main.model.generic.CombinatorialMultiObjectiveOptimizationAlgorithm;
+import main.model.generic.InterfaceRandom;
+
+/**
+ * Controlleur principal
+ * @author Corentin Pontal
+ *
+ */
 public class Controller {
+	
+	@FXML
+	private BorderPane paneCentral;
+	
+	@FXML
+	private void initialize() {
+		ControllerSimulation customControl = new ControllerSimulation();
+		Pane view = customControl.getPaneControllerSimulation();
+		paneCentral.setCenter(view);
+	}
+	
+	@FXML
+	private void actionRetour(ActionEvent Evt) {
+		System.out.println("Le clic fonctionne bien");
+	}
+	
+	@FXML
+	private void actionSuivant(ActionEvent Evt) {
+		System.out.println("Le clic fonctionne bien");
+	}
+	
 }
