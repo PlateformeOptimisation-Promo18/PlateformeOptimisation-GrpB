8886f5d1ef8beef08a1be01e4b7dc591e465a4e1
Simon <simon.marques@limayrac.fr>
2020-06-01 14:53:44 +0200

    Fin projet

--- src/main/model/algorithmes/spea2/Archive.java
+++ src/main/model/algorithmes/spea2/Archive.java
@@ -1,7 +1,19 @@
 package main.model.algorithmes.spea2;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+import model.generic.InterfaceRandom;
+import model.generic.ParetoFront;
+import model.generic.Problem;
+
 /*
- @autor : Simon Marquès
+ @autor : Simon Marquès et Sophie Pasdeloup
  @create : 09/05/2020
  @update : 31/05/2020
  @version : 1.0
@@ -9,24 +21,144 @@ package main.model.algorithmes.spea2;
 
 public class Archive extends Population {
 
+    /**
+     * Constructeur de la classe Archive
+     * @param isArchiveSize taille de l'archive
+     */
     public Archive(int isArchiveSize){
 
         this.taille = isArchiveSize;
     }
 
+    /**
+     * Met à jour l'archive en rajoutant les meilleur d'une nouvelle population
+     * @param population Nouvelle population à étudier
+     * @param iArchiveSize Taille max de l'archive
+     * @param pb Problem concerné
+     */
     public void updateArchive(Population population, int iArchiveSize, Problem pb) {
+        int iSizePop = population.getIndividualSet().size();
+        ParetoFront pareto = new ParetoFront();
 
+        //calcul force
+        calculForce(population, iSizePop, pb);
+
+        //calcul rawFitness
+        calculRawFitness(population, iSizePop, pb);
+
+        //calcul density
+        calculDensity(population, iSizePop, iArchiveSize, pb, pareto);
+
+        //calcul fitness
+        for(Individual individual : population.getIndividualSet()) {
+            individual.setFitness(individual.getRawFitness() + individual.getDensity());
+        }
+
+        Collections.sort(population.getIndividualSet());
+
+        PriorityQueue<Individual> queue = new PriorityQueue<>(iSizePop);
+        queue.addAll(set);
+
+        for(int i = 0 ; i<iSizePop && population.getIndividual(i).getFitness() < 1 ; i++) {
+            this.set.add(queue.poll());
+        }
+
+        if(this.set.size() > iArchiveSize) {
+            pareto.reduceIfNecessary(iArchiveSize, pb);
+        }else {
+            for(int i = 0 ; i < iArchiveSize-this.set.size() ; i++) {
+                this.add(queue.poll());
+            }
+        }
     }
 
+    /**
+     * Calcul la force de chaque individu d'une population
+     * @param population Population d'individus dont on doit calculer la force
+     * @param iSizePop Taille de la population
+     * @param pb Problème concerné
+     */
+    private void calculForce(Population population, int iSizePop, Problem pb) {
+        Individual ind1;
+        int force = 0;
+        for(int i = 0 ; i<iSizePop ; i++) {
+            ind1 = population.getIndividual(i);
+            for (int j = 0 ; j<iSizePop ; j++) {
+                if(ParetoFront.domine(ind1.getSolution(), population.getIndividual(j).getSolution(), pb)) {
+                    force += 1;
+                }
+            }
+            ind1.setForce(force);
+        }
+    }
+
+    /**
+     * Calcul le rawFitness de chaque individu d'une population
+     * @param population Population d'individus dont on doit calculer le rawFitness
+     * @param iSizePop Taille de la population
+     * @param pb Problème concerné
+     */
+    private void calculRawFitness(Population population, int iSizePop, Problem pb) {
+        Individual ind1;
+        int rawFitness = 0;
+        for(int i = 0 ; i<iSizePop ; i++) {
+            ind1 = population.getIndividual(i);
+            for (int j = 0 ; j<iSizePop ; j++) {
+                if(ParetoFront.domine(population.getIndividual(j).getSolution(),ind1.getSolution(), pb)) {
+                    rawFitness += ind1.getForce();
+                }
+            }
+            ind1.setRawFitness(rawFitness);
+        }
+    }
+
+    /**
+     * Calcul la densité de chaque individus d'une population
+     * @param population Population d'individu dont on doit calculer la densité
+     * @param iSizePop Taille de population
+     * @param iArchiveSize Taile de l'archive
+     * @param pb Porbleme concerné
+     * @param pareto Front de pareto
+     */
+    private void calculDensity(Population population, int iSizePop, int iArchiveSize, Problem pb, ParetoFront pareto) {
+        int dKIndividu = (int) Math.sqrt(iSizePop + iArchiveSize);
+        PriorityQueue<Individual> test = new PriorityQueue<>();
+        for(Individual ind : population.getIndividualSet()) {
+            test.add(ind);
+        }
+        double[] dDistance = new double[iSizePop];
+        for (int i = 0 ; i<iSizePop ; i++) {
+            for(int j = i ; j < iSizePop ; j++) {
+                dDistance[j] = pareto.getDistance(population.getIndividual(i).getSolution(), population.getIndividual(j).getSolution(), pb);
+            }
+            double dDensity = 1/(dDistance[dKIndividu] + 2);
+            population.getIndividual(i).setDensity(dDensity);
+        }
+    }
+
+    /**
+     *
+     * @return la configuration de la population
+     */
     private List<Individual> getSet(){
 
         return set;
     }
 
+    /**
+     * Mise à jour de la population
+     * @param pb
+     * @param populationTemp
+     */
     private void miseAjourDistancePop(Problem pb, Population populationTemp) {
+
         set.add(populationTemp);
     }
 
+
+    /**
+     * Enlève les individu en double de l'archive
+     */
     public void enleveDoublons(){
         int compteur;
         int i;
@@ -41,6 +173,13 @@ public class Archive extends Population {
         }
     }
 
+    /**
+     * Selection d'élément dans la population
+     * @param population population concerné
+     * @param generator
+     * @param iPopSize
+     * @param iNumkVoisin
+     */
     public void selection(Population population, InterfaceRandom generator, int iPopSize, int iNumkVoisin) {
         int compteur;
         for(compteur=0;compteur<taille;compteur++){
@@ -51,6 +190,10 @@ public class Archive extends Population {
         }
     }
 
+    /**
+     * Selection des parents dans la population courante
+     * @param nombreParent indice de où se situe les parents dans la population
+     */
     public Population selectionParents(int nombreParent){
         Population popParents = new Population(nombreParent);
         int compteur;
@@ -60,6 +203,10 @@ public class Archive extends Population {
         return populationParent;
     }
 
+    /**
+     * Sélection de l'archive
+     * @param population population concerné
+     */
     public void SelectionArchive(Population population){
         for(int i = 0 ; i < this.set.size() ; i ++) {
             if(this.set.isEmpty()) {
--- src/main/model/algorithmes/spea2/Population.java
+++ src/main/model/algorithmes/spea2/Population.java
@@ -1,6 +1,5 @@
 package main.model.algorithmes.spea2;
 
-import javafx.beans.InvalidationListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,10 +7,9 @@ import java.util.List;
 import model.generic.InterfaceRandom;
 import model.generic.Problem;
 
-import main.model.generic.Solution;
 
 /*
- @autor : Simon Marquès
+ @autor : Simon Marquès et Sophie Pasdeloup
  @create : 09/05/2020
  @update : 31/05/2020
  @version : 1.0
@@ -21,29 +19,50 @@ public class Population{
     private int taille;
     private List<Individual> set;
 
+    /**
+     * Constructeur de la classe Population
+     * @param iSize taille  laquelle la liste d'individu de population est initialisé
+     */
     public Population(int iSize){
 
         this.set = new ArrayList<Individual>();
     }
 
+    /**
+     * Retourne le nombre d'individus de la population
+     * @return nombre d'individu de la population
+     */
     public int getNbIdividual() {
         return this.set.size();
     }
 
+    /**
+     * Ajoute à la population un individu
+     * @param individual individu à ajouter
+     */
     protected void add(Individual ind){
         this.set.add(ind);
     }
 
+    /**
+     * Initialise la population
+     * @param iPopulationSize nombre d'individu
+     * @param pb Problème concerné
+     * @param generator Générateur de nombre aléatoire
+     */
     public void initialise(int iPopulationSize, Problem pb, InterfaceRandom generator) throws Exception{
-        int i;
-        Solution tabSolution[] = new Solution();
-        for(i=0;i<=iPopulationSize;i++){
-            Solution staticSolution = pb.GetSolution();
-            staticSolution->randomSetValues();
-            tabSolution[i] = staticSolution;
+        int compteur;
+        for(compteur=0;compteur<=iPopulationSize;compteur++){
+            Individual ind = new Individual(pb);
+            ind.randomSetValue(pb,generator);
+            this.set.add(ind);
         }
     }
 
+    /**
+     * Evalue chaque individu de la population
+     * @param pb Problème concerné
+     */
     public void evalue(Problem pb) {
         int compteur;
         for(compteur=0;compteur<taille;i++){
@@ -55,17 +74,56 @@ public class Population{
         }
     }
 
+    /**
+     * Croisement entre deux parents tirés aléatoirement
+     * @param dCrossoverRate Probabilité de croisements des individus
+     * @param pb Probléme concerné
+     * @param generator Générateur de nombre aléatoire
+     * @return la nouvelle population de croisement
+     */
     public Population crossover(double dCrossoverRate, Problem pb, InterfaceRandom generator) {
-        for(Individual individual : this.set) {
-            individual.crossoverUniform(individual, pb, generator);
+        int iIndexIndividual;
+        int iRandom;
+        Individual ind1 = new Individu(pb);
+        do {
+            iIndexIndividual = generator.nextInt(this.set.size());
+            iRandom = generator.nextInt(1);
+            ind1 = this.set.get(iIndexIndividual);
         }
+        while(iRandom > dCrossoverRate);
+        Individual ind2 = this.tireParent(ind1, generator);
+
+        ind1.crossoverUniform(ind2, pb, generator);
+
         return this;
     }
 
+    /**
+     * Tire un parent au hazard dans la population différent de ind1 pour faire un croisement
+     * @param ind1 premier individu du croisement
+     * @param generator de nombre aléatoire
+     * @return le 2ème parent pour le crossover
+     */
     private Individual tireParent(Individual ind1, InterfaceRandom generator){
-        return ind1;
+        int iIndex;
+        Individual ind2 = new Individu();
+        do {
+            iIndex = generator.nextInt(this.set.size());
+            ind2 = this.set.get(iIndex);
+        }
+        while(ind1.equals(ind2));
+        return ind2;
     }
 
+    /**
+     * Mute de parent en fonction de la probabilité de mutation, de l'individu
+     * et la probabilité de mutation
+     * @param childreen Population enfant créé par mutation
+     * @param dGeneMutation Probabilité de mutation
+     * @param dIndividualMutationRate Probabilité de mutation d'un individu
+     * @param pb Problème concerné
+     * @param generator Générateur de nombre aléatoire
+     */
     public void mutation(Population childreen, double dGeneMutation, double dIndividualMutationRate, Problem pb) {
         for(int i = 0 ; i < childreen.getIndividualSet().size() ; i++) {
             Individual individual = childreen.getIndividual(i);
@@ -81,26 +139,21 @@ public class Population{
         }
     }
 
-    public int Individual getNbIndividuals(int i) {
-
-        return set.size(get(i));
-    }
-
+    /**
+     * Clear l'objet courant
+     */
     public void clear() {
 
         set.clear();
     }
 
     public List<Individual> getIndividualSet(){
+
         return this.set;
     }
 
     public void setIndividualSet(List<Individual> set) {
         this.set = set;
-    }
-
-    public List<Solution> getAssociatedSolutionSet(){
-
     }
 
     public Individual getIndividual(int i) {
